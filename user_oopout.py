843b216f7bc607705405424ab08e99a57ff96247
Your Name <you@example.com>
2023-01-24 23:25:28 -0600

    Task5

---
+++ user_oopout.py
@@ -0,0 +1,72 @@
+
+Welcome, I'm a new PyBuddy.
+
+I'm Jakiel.
+I'm a Species.CAT with 4 legs.
+I weigh 8.12 lbs.
+I've been alive for 0:00:00.000904.
+I'm available for tutoring.
+I know:
+  - Tableau
+  - GitHub
+  - Python
+  - R
+  - VS Code
+
+
+
+        You'll need curiousity, the ability to search the web,
+        and the tenacity and resourcefulness
+        to solve all kinds of challenges.
+
+        Let's get started!
+
+        
+
+Welcome, I'm a new PyBuddy.
+
+I'm Diana.
+I'm a Species.DOG with 4 legs.
+I weigh 10.44 lbs.
+I've been alive for 0:00:00.000017.
+I'm available for tutoring.
+I know:
+  - Git
+  - R
+  - Python
+  - Markdown
+  - VS Code
+
+
+
+        You'll need curiousity, the ability to search the web,
+        and the tenacity and resourcefulness
+        to solve all kinds of challenges.
+
+        Let's get started!
+
+        
+
+Welcome, I'm a new PyBuddy.
+
+I'm Brooklyn.
+I'm a Species.ELF with 4 legs.
+I weigh 8.00 lbs.
+I've been alive for 0:00:00.000015.
+I'm already helping others learn Python.
+I know:
+  - Git
+  - GitHub
+  - SQL
+  - Markdown
+  - Excel
+
+
+
+        You'll need curiousity, the ability to search the web,
+        and the tenacity and resourcefulness
+        to solve all kinds of challenges.
+
+        Let's get started!
+
+        
